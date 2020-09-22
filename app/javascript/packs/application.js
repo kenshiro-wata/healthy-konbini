d15661afd29122aec2a205affedc1ec33dd098d7
kenshiro-wata <kenshiroooo1234@gmail.com>
2020-09-22 20:07:36 +0900

    修正２

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 //=require ("jquery3")
 //=require ("popper")
 //=require ("bootstrap")
-require ("Chart.min")
+//=require ("Chart.min")
 
 require("@rails/ujs").start()
 require("turbolinks").start()
